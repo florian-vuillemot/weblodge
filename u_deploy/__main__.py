4337d34b099958fac155253fd123ec4c92f61c1d
Vuillemot Florian <vuillemot.florian@outlook.fr>
2023-06-24 19:24:45 +0200

    Deploy an application on an azure webapp

--- u_deploy/__main__.py
+++ u_deploy/__main__.py
@@ -1,20 +1,6 @@
-from ._azure import Cli
-from ._azure import ResourceGroupHelper, AppServiceHelper, WebAppHelper
+from .build import WebApp as WebAppBuilder
+from .deploy import WebApp as WebAppDeploy
 
-cli = Cli()
-rg = ResourceGroupHelper(cli)
-ap = AppServiceHelper(cli)
-wa = WebAppHelper(cli)
-
-# _rg = rg.create('develop-tmp', 'northeurope')
-# _ap = ap.create('app-develop-tmp', 'B1', _rg)
-# _wa = wa.create('webapp-develop-tmp', _ap)
-
-_rg = rg.get('develop-tmp', 'northeurope')
-_ap = ap.get('app-develop-tmp', _rg)
-_wa = wa.get('webapp-develop-tmp')
-
-
-wa.delete(_wa)
-ap.delete(_ap)
-rg.delete(_rg)
+build = WebAppBuilder()
+build.build()
+WebAppDeploy().deploy(build.package_path)
