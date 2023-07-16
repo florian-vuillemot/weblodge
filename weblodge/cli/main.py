49453cfc4978079b3b0ada218b2cb81842ee4487
Vuillemot Florian <florian.vuillemot@avaiga.com>
2023-07-16 08:30:05 +0200

    Move CLI args tests

--- weblodge/cli/main.py
+++ weblodge/cli/main.py
@@ -8,60 +8,25 @@ and calls the appropriate functions.
 """
 import sys
 import logging
-import argparse
 
 from typing import Dict
 
 import weblodge.state as state
-
-from weblodge.parameters import Parser, ConfigIsNotDefined, ConfigIsDefined
 from weblodge.web_app import WebApp
+from weblodge.parameters import Parser, ConfigIsNotDefined, ConfigIsDefined
+
+from .args import get_cli_args, CLI_NAME
 
 
 logger = logging.getLogger('weblodge')
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-# Command Line Interface name.
-CLI_NAME = sys.argv[0]
-
-
-def get_weblodge_parameters():
-    """
-    Return default parameters.
-    """
-    # If run user run: `python weblodge.py -h` or `python weblodge.py --help`
-    # then we want to display the **WebLodge** `help` message.
-    # Otherwise, we want to parse the arguments and better scope the help message.
-    # Example: `python weblodge.py build -h` must print the `help` message for the `build` action.
-    asking_global_help = len(sys.argv) == 2 and '-h' in sys.argv[1]
-
-    _parser = argparse.ArgumentParser(
-        description='Deploy a Python Flask-based application to Azure.',
-        add_help=asking_global_help
-    )
-    _parser.add_argument(
-        'action',
-        type=str,
-        help='Action to perform.',
-        choices=['build', 'deploy', 'delete', 'logs']
-    )
-    _parser.add_argument(
-        '--config-filename',
-        type=str,
-        help='File containing the deployment configuration.',
-        default='.weblodge.json',
-        required=False
-    )
-    args, _ = _parser.parse_known_args()
-
-    return args.action, args.config_filename
-
 
 # pylint: disable=missing-function-docstring
 def main():
     parameters = Parser()
-    action, config_filename = get_weblodge_parameters()
+    action, config_filename = get_cli_args()
     web_app = WebApp(parameters.load)
 
     config = state.load(config_filename)
