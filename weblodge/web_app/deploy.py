9e7422050345e874cc5a05663e6953f141f1526c
Vuillemot Florian <florian.vuillemot@avaiga.com>
2023-07-26 11:43:59 +0200

    try with 60sec

--- weblodge/web_app/deploy.py
+++ weblodge/web_app/deploy.py
@@ -119,7 +119,7 @@ def deploy(config: DeploymentConfig) -> AzureWebApp:
     set_webapp_env_var(web_app, config.env_file)
 
     import time
-    time.sleep(30)
+    time.sleep(60)
 
     logger.info('Uploading the application...')
     web_app.deploy(os.path.join(config.dist, config.package))
